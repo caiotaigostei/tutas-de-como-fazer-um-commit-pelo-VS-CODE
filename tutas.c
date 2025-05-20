17244073765531e1b650b155e79e3451a4f2ac4c
Caio Cordeiro <166307239+caiotaigostei@users.noreply.github.com>
2025-05-20 17:37:49 -0300

    Create tutas.c

---
+++ tutas.c
@@ -0,0 +1,19 @@
+//Fazer commit no github pelo Vs Code:
+
+//1)'git init' no terminal do Vs Code -> GIT INIT inicia um repositorio local
+//2)'git add .' no terminal do Vs Code
+//3)'git commit -m "comentário qualquer"' no terminal do Vs Code
+//4)Depois com isso, siga para o github, crie um repositorio no seu perfil, com isso copie o link do seu repositorio recém criado
+//5)'git remote add origin <link URL do seu recém repositorio>'
+//6)'git branch -M main'no terminal do Vs Code
+//7)'git push -u origin main'
+
+//Com isso seu código estará salvo no github!
+
+//Novos commits no github pelo Vs Code:
+
+//1)'git add .' no terminal do Vs Code
+//2)'git commit -m "novos comentários"' no terminal do Vs Code
+//3)'git push'
+
+//Com isso seu código atualizado estará salvo no github!
